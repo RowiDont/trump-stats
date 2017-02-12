1b3b645fa65783963cac8ac15e6a29b5e21f05c0
Rafael Patel <rafiepatel@gmail.com>
2017-02-12 17:38:16 -0500

    Add changes to work with production env.

--- app.js
+++ app.js
@@ -33,7 +33,8 @@ app.get('/', function (req, res) {
     render(res)
   } else {
     fetchAndSetTrumpMetrics()
-    .then(parser.parseCsv(metrics)
+    .then(
+      parser.parseCsv(metrics)
       .then(data => { metrics.obama = data })
       .catch(err => handleError(res, err))
     )
@@ -42,6 +43,6 @@ app.get('/', function (req, res) {
   }
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+const serverPort = process.env.OPENSHIFT_NODEJS_PORT || 3000
+
+app.listen(serverPort, () => { console.log(`Server listening on port ${serverPort}`) })
