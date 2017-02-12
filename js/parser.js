f3ef34b5ed5bb491472c00eb9bd1aa9dc07ca28f
Rafael Patel <rafiepatel@gmail.com>
2017-02-12 15:11:32 -0500

    Tidy up files, fix linter errors.

--- js/parser.js
+++ js/parser.js
@@ -2,9 +2,9 @@ const csv = require('csv-parse')
 const fs = require('fs')
 
 module.exports = {
-  parseCsv: function(metrics) {
-    return new Promise(function(resolve, reject) {
-      const fileName = `${process.cwd()}/assets/obama-job-approval.csv`;
+  parseCsv: function (metrics) {
+    return new Promise(function (resolve, reject) {
+      const fileName = `${process.cwd()}/assets/obama-job-approval.csv`
 
       fs.readFile(fileName, (err, data) => {
         if (err) reject(err)
